024a8428c47f27bf14e128b5523de17f348eeaa2
BT_1N3_09 <mmoore4262>
2018-10-05 09:29:34 -0400

    runner change

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,11 @@
 public class Runner {
-    private static String[] firstNames=["goo","gaa"];
-    private static String[] familyNames=["last","last2"];
+    private static String[] firstNames={"goo","gaa"};
+    private static String[] familyNames={"last","last2"};
+
+    public static void main (String[] args)
+    {
+        Teacher 
+    }
 
 
 
